87c0d87c6f2f1065b1d1e764ac37964b56bbf318
Felipe Pereira <felp.pereira98@gmail.com>
2021-01-26 15:39:12 -0300

    Formatting age field

--- instructors.js
+++ instructors.js
@@ -1,27 +1,42 @@
-const fs = require ("fs")
+const fs = require("fs");
 
 const data = require("./data.json");
 
 //show
-exports.show = function(req,res){
-    const { id } = req.params
+exports.show = function (req, res) {
+  const { id } = req.params;
 
-    const foundInstructor = data.instructors.find(function(instructor){
-        return instructor.id == id
-    })
+  const foundInstructor = data.instructors.find(function (instructor) {
+    return instructor.id == id;
+  });
 
-    if(!foundInstructor) return res.send("Instrucotr not-found")
+  if (!foundInstructor) return res.send("Instrucotr not-found");
 
-    const instructor = {
+  function age(timestamp) {
+    const today = new Date()
+    const birthDate = new Date(timestamp)
 
-      ...foundInstructor,
-      age:"",
-      services: foundInstructor.services.split(","),
-      created_at:"",
+    let age = today.getFullYear() - birthDate.getFullYear()
+    const month = today.getMonth() - birthDate.getMonth()
+
+    if (month < 0 ||
+       (month == 0 && 
+        today.getDate() <= birthDate.getDate())) {
+      age = age - 1;
     }
 
-    return res.render("instructors/show", {instructor})
-}
+    return age
+  }
+
+  const instructor = {
+    ...foundInstructor,
+    age: age(foundInstructor.birth),
+    services: foundInstructor.services.split(","),
+    created_at: "",
+  };
+
+  return res.render("instructors/show", { instructor });
+};
 
 // create
 exports.post = function (req, res) {
@@ -33,30 +48,28 @@ exports.post = function (req, res) {
     }
   }
 
- let { avatar_url, birth, name, services, gender } = req.body;
+  let { avatar_url, birth, name, services, gender } = req.body;
 
-
-birth = Date.parse(birth)
-const created_at = Date.now()
-const id = Number(data.instructors.length + 1)
-
+  birth = Date.parse(birth);
+  const created_at = Date.now();
+  const id = Number(data.instructors.length + 1);
 
   data.instructors.push({
-      id,
-      avatar_url,
-      name,
-      birth,
-      gender,
-      services,
-      created_at
+    id,
+    avatar_url,
+    name,
+    birth,
+    gender,
+    services,
+    created_at,
   });
 
   fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
     if (err) return res.send("Write file error!");
 
-    return res.redirect("/instructors")
-  })
-}
+    return res.redirect("/instructors");
+  });
+};
 
 // update
 
